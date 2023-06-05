afbfd57bb0bff3296bbcdc27981fa17a586029d6
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-05 18:51:15 -0300

    git

--- Aula01CriandoOprimeiroBancoDeDados.sql
+++ Aula01CriandoOprimeiroBancoDeDados.sql
@@ -1,1 +1,13 @@
-CREATE DATABASE cadatro
+
+CREATE DATABASE IF NOT EXISTS cadastro;
+
+use cadastro;
+
+CREATE TABLE pessoas (
+    nome varchar(30),
+    idade int,
+    sexo char(1),
+    peso float,
+    altura float,
+    nacionalidade varchar(20)
+);
